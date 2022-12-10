813de3ebf687c1be6d377e654638e94c60b16082
macedito <aisha.macedo@tecsup.edu.pe>
2022-12-10 01:31:26 -0500

    Initial Commit

---
+++ Mensajitos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mensajitos
+//
+//  Created by Aisha Belen Macedo Zeballos on 10/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
